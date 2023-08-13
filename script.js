5747f3eb916231980231448dbb8d9fc8b8293358
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-08-14 09:59:30 +1000

    Render gameboard array onto webpage

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
-const gameboard = (() => {
-  let firstRow = ["x", "x", "x"];
-  let secondRow = ["0", "0", "0"];
-  let thirdRow = ["x", "x", "x"];
-  return { firstRow, secondRow, thirdRow };
+const gameBoard = (() => {
+  let gameArray = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
+  return { gameArray };
 })();
 
-const displayController = (() => {
+const players = () => {
+  let people = ["playerX", "playerO"];
+
+  return { people };
+};
+
+const renderGameBoard = (() => {
   let numberOfSquares = 3;
 
   // cacheDOM
   const gridSquares = document.querySelector("#game-board");
 
   // render
-  (function createSquares(numberOfSquares, gridSquares) {
+  (function createSquares(numberOfSquares, gridSquares, gameBoard) {
+    let index = 0;
     for (let i = 0; i < numberOfSquares; i++) {
       const column = document.createElement("div");
       column.setAttribute("class", "column");
@@ -21,10 +26,13 @@ const displayController = (() => {
       for (let j = 0; j < numberOfSquares; j++) {
         const row = document.createElement("div");
         row.setAttribute("class", "grids");
+        row.textContent = gameBoard.gameArray[index];
         column.appendChild(row);
+
+        index += 1;
       }
     }
-  })(numberOfSquares, gridSquares);
+  })(numberOfSquares, gridSquares, gameBoard);
 })();
 
-const player = () => {};
+const displayController = () => {};
