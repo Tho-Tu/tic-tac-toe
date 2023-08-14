76ddc97ad817025b51c756e89fb40f82b9c69b68
Tho Tu <126043463+Tho-Tu@users.noreply.github.com>
2023-08-14 15:18:31 +1000

    Organise and place code into modules

--- script.js
+++ script.js
@@ -1,22 +1,65 @@
-const gameBoard = (() => {
-  let gameArray = ["x", "x", "x", "x", "x", "x", "x", "x", "x"];
-  return { gameArray };
-})();
+const Cell = () => {
+  let value = null;
 
-const players = () => {
-  let people = ["playerX", "playerO"];
+  const addMove = (player) => {
+    value = player;
+  };
 
-  return { people };
+  const getValue = () => value;
+
+  return {
+    addMove,
+    getValue,
+  };
 };
 
-const renderGameBoard = (() => {
+const Gameboard = (function () {
+  const rows = 3;
+  const columns = 3;
+  const board = [];
+
+  for (let i = 0; i < rows; i++) {
+    board[i] = [];
+    for (let j = 0; j < columns; j++) {
+      board[i].push(Cell.getValue);
+    }
+  }
+
+  const getBoard = () => board;
+
+  const playMove = (row, column, player) => {
+    board[row][column].addMove(player);
+  };
+
+  return { getBoard, playMove };
+})();
+
+const Players = () => {
+  let players = ["X", "O"];
+  return { players };
+};
+
+// flow of game
+const GameController = (players) => {
+  let activeTurn = players[0];
+
+  const switchTurns = () => {
+    activeTurn === players[0] ? players[1] : players[0];
+  };
+  const getPlayerTurn = () => activeTurn;
+
+  return { switchTurns, getPlayerTurn };
+};
+
+// visual representation
+const DisplayController = (function () {
   let numberOfSquares = 3;
 
   // cacheDOM
   const gridSquares = document.querySelector("#game-board");
 
   // render
-  (function createSquares(numberOfSquares, gridSquares, gameBoard) {
+  (function createSquares(numberOfSquares, gridSquares, Gameboard) {
     let index = 0;
     for (let i = 0; i < numberOfSquares; i++) {
       const column = document.createElement("div");
@@ -26,13 +69,11 @@ const renderGameBoard = (() => {
       for (let j = 0; j < numberOfSquares; j++) {
         const row = document.createElement("div");
         row.setAttribute("class", "grids");
-        row.textContent = gameBoard.gameArray[index];
+        row.textContent = Gameboard.getBoard[index];
         column.appendChild(row);
 
         index += 1;
       }
     }
-  })(numberOfSquares, gridSquares, gameBoard);
+  })(numberOfSquares, gridSquares, Gameboard);
 })();
-
-const displayController = () => {};
